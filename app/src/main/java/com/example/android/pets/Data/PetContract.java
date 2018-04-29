81da6871836377b1e5663b99d07dda026b1232d2
Gustavo Trevisan Dini <gustavotrevisandini@gmail.com>
2018-04-29 20:36:36 -0300

    After Lesson 14

--- app/src/main/java/com/example/android/pets/Data/PetContract.java
+++ app/src/main/java/com/example/android/pets/Data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.Data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,9 +9,17 @@ import android.provider.BaseColumns;
 
 public final class PetContract {
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PETS = "pets";
+
     private PetContract(){}
 
     public static final class PetEntry implements BaseColumns{
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public final static String TABLE_NAME = "pets";
 
